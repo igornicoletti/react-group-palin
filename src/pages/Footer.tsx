cfd18ca8ec59b3a5e349d7d25be1d00034dae4cb
igornicoletti <igor93nicoletti@gmail.com>
2025-02-03 14:48:34 -0300

    feat: header

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -42,22 +42,23 @@ export const Footer = () => {
   return (
     <footer className="bg-gray-900">
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
-        <nav aria-label="Footer" className="-mb-6 flex flex-wrap justify-center gap-x-12 gap-y-3 text-sm/6">
+        <img alt="" src="/images/palin-martins-white.png" className="h-20 w-auto mx-auto" />
+        <nav aria-label="Footer" className="mt-6 flex flex-wrap justify-center gap-x-12 gap-y-3 text-sm/6">
           {navigation.main.map((item) => (
             <a key={item.name} href={item.href} className="text-dracula-white/75 hover:text-dracula-white">
               {item.name}
             </a>
           ))}
         </nav>
-        <div className="mt-16 flex justify-center gap-x-10">
+        <div className="mt-10 flex justify-center gap-x-10">
           {navigation.social.map((item) => (
             <a key={item.name} href={item.href} className="text-dracula-white/75 hover:text-dracula-white" target='_blank'>
               <span className="sr-only">{item.name}</span>
-              <item.icon aria-hidden="true" weight="duotone" className="size-6 text-dracula-white/50 hover:text-dracula-main" />
+              <item.icon aria-hidden="true" weight="duotone" className="size-6 text-dracula-white/75 hover:text-dracula-main" />
             </a>
           ))}
         </div>
-        <p className="mt-10 text-center text-sm/6 text-dracula-white/50">&copy; 2025 Palin & Martins. Todos os direitos reservados.</p>
+        <p className="mt-14 text-center text-sm/6 text-dracula-white/50">&copy; 2025 Palin & Martins. Todos os direitos reservados.</p>
       </div>
     </footer>
   )
--- src/pages/HeaderNav.tsx
+++ src/pages/HeaderNav.tsx
@@ -1,6 +1,6 @@
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { List, X } from '@phosphor-icons/react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const navigation = [
   { name: 'Empresa', href: '#' },
@@ -9,65 +9,78 @@ const navigation = [
 
 export const HeaderNav = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [isHeaderVisible, setIsHeaderVisible] = useState(true)
+
+  useEffect(() => {
+    let lastScrollY = 0
+    const handleScroll = () => {
+      setIsHeaderVisible(window.scrollY <= lastScrollY || window.scrollY <= 100)
+      lastScrollY = window.scrollY
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
 
   return (
-    <header className="bg-dracula-white">
+    <header className={`z-10 bg-dracula-white fixed top-0 left-0 shadow-md shadow-dracula-main/5 w-full transition-transform duration-300 ${!isHeaderVisible ? '-translate-y-full' : 'translate-y-0'}`}>
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-        <a href="#" className="">
-          <span className="sr-only">Grupo Palin & Martins</span>
-          <img alt="" src="/images/palin-martins-black.png" className="h-12 w-auto" />
-        </a>
-        <div className="flex lg:hidden">
-          <button type="button" onClick={() => setMobileMenuOpen(true)} className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-dracula-dark/75" >
-            <span className="sr-only">Open main menu</span>
-            <List aria-hidden="true" className="size-6 text-dracula-main" weight="duotone" />
-          </button>
-        </div>
-        <div className="hidden lg:flex items-center gap-x-12">
-          <div className="flex gap-x-12">
+        <div className="flex items-center gap-x-16">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Grupo Palin & Martins</span>
+            <img alt="" src="/images/palin-martins-black.png" className="h-14 w-auto" />
+          </a>
+          <div className="hidden lg:flex gap-x-16 -mb-1">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm/6 font-medium text-dracula-dark/75">
+              <a key={item.name} href={item.href} className="text-sm/6 font-medium text-dracula-dark/75 hover:text-dracula-dark">
                 {item.name}
               </a>
             ))}
           </div>
-          <div className="flex gap-x-6">
-            <a href="#" className="px-4 py-1.5 text-sm/6 font-medium border border-dracula-main text-dracula-main rounded-full">
-              Trabalhe conosco
-            </a>
-            <a href="#" className="px-4 py-1.5 text-sm/6 font-medium bg-dracula-main text-dracula-white rounded-lg">
-              Entre em contato
-            </a>
-          </div>
+        </div>
+        <div className="flex lg:hidden">
+          <button type="button" onClick={() => setMobileMenuOpen(true)} className="-m-2.5 p-2.5">
+            <span className="sr-only">Open main menu</span>
+            <List aria-hidden="true" className="size-6 text-dracula-main" weight="duotone" />
+          </button>
+        </div>
+        <div className="hidden lg:flex items-center gap-x-8 -mb-1">
+          <a href="#" className="text-sm/6 font-semibold text-dracula-main">
+            Trabalhe conosco
+          </a>
+          <a href="#" className="px-4 py-1.5 text-sm/6 font-semibold border border-dracula-main text-dracula-main rounded-lg">
+            Entre em contato
+          </a>
         </div>
       </nav>
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
         <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-dracula-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-dracula-dark/10">
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-dracula-white p-6 sm:max-w-sm sm:ring-1 sm:ring-dracula-dark/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Grupo Palin & Martins</span>
-              <img alt="" src="/images/palin-martins-black.png" className="h-12 w-auto" />
+              <img alt="" src="/images/palin-martins-black.png" className="h-14 w-auto" />
             </a>
-            <button type="button" onClick={() => setMobileMenuOpen(false)} className="-m-2.5 rounded-md p-2.5 text-dracula-dark" >
+            <button type="button" onClick={() => setMobileMenuOpen(false)} className="-m-2.5 p-2.5">
               <span className="sr-only">Close menu</span>
-              <X aria-hidden="true" className="size-6 text-dracula-dark" weight="duotone" />
+              <X aria-hidden="true" className="size-6 text-dracula-main" weight="duotone" />
             </button>
           </div>
           <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-dracula-dark/10">
-              <div className="space-y-2 py-6">
+            <div className="divide-y divide-dracula-dark/10">
+              <div className="py-6">
                 {navigation.map((item) => (
-                  <a key={item.name} href={item.href} className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-dracula-dark hover:bg-dracula-dark/5" >
+                  <a key={item.name} href={item.href} className="-mx-3 block rounded-lg px-3 py-2 text-sm/6 font-medium text-dracula-dark hover:bg-dracula-dark/5" >
                     {item.name}
                   </a>
                 ))}
               </div>
-              <div className="flex flex-col gap-x-6  py-6">
-                <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-dracula-dark hover:bg-dracula-dark/5">
+              <div className="flex flex-col gap-x-6 py-6">
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-sm/6 font-semibold text-dracula-dark hover:bg-dracula-dark/5">
                   Trabalhe conosco
                 </a>
-                <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-dracula-dark hover:bg-dracula-dark/5">
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-sm/6 font-semibold text-dracula-main hover:bg-dracula-dark/5">
                   Entre em contato
                 </a>
               </div>
