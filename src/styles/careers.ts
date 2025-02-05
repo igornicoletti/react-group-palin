6602043af1ee65e7c335bcf2bc3a4db591028216
igornicoletti <igor93nicoletti@gmail.com>
2025-02-05 03:12:11 -0300

    Refactor styles: optimize and organize Tailwind classes

--- src/styles/careers.ts
+++ src/styles/careers.ts
@@ -2,14 +2,14 @@ import { tv } from 'tailwind-variants'
 
 export const careersVariants = tv({
   slots: {
-    section: " py-24 sm:py-32 mx-auto max-w-7xl px-6 lg:px-8",
-    gridContainer: "flex flex-col lg:flex-row gap-24 items-center",
+    section: "py-24 sm:py-32 mx-auto max-w-7xl px-6 lg:px-8",
+    gridContainer: "flex flex-col lg:flex-row space-x-24 space-y-24 items-center justify-between",
     textContainer: "relative",
     sectionLabel: "text-lg/7 font-semibold text-dracula-main",
-    title: "mt-2 max-w-3xl text-4xl font-semibold tracking-tight text-pretty text-dracula-dark",
-    subtitleContainer: "mt-6 space-y-2 max-w-3xl text-xl/8 text-balance font-extralight text-dracula-dark",
+    title: "mt-2 lg:max-w-lg text-4xl font-semibold tracking-tight text-pretty text-dracula-dark",
+    subtitleContainer: "mt-6 space-y-2 lg:max-w-lg text-xl/8 text-balance font-extralight text-dracula-dark",
     imageContainer: "relative flex items-center justify-center",
     imageBackground: "absolute text-dracula-main/25 w-full",
-    image: "mx-auto w-full max-w-xl rounded-xl shadow-xl shadow-dracula-dark/10 rotate-3"
+    image: "mx-auto w-full max-w-2xl rounded-xl shadow-xl shadow-dracula-dark/10 rotate-3"
   }
 })
